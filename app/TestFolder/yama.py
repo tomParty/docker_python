97be06dc6a1c4c1fdf7b6a49d0bcb835a8274019
Tom <thomasbkruth@gmail.com>
2018-06-11 22:24:25 -0700

    remote debugging of python (flask) inside docker container finished

---
+++ app/TestFolder/yama.py
@@ -0,0 +1,5 @@
+def YamaTest():
+    name = 'test'
+    yama = 'this is hopefully working'
+    but = 'is this going to work'
+    return name + ' ' + yama + ' ' + but
--- app/app.py
+++ app/app.py
@@ -1,26 +1,28 @@
 from flask import Flask
 from flask_restful import Api
+from TestFolder.yama import YamaTest 
 
 
 try:
     print("Hello")
-    import socket
+    # import socket
     import ptvsd
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.close()
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock.close()
     ptvsd.enable_attach(secret=None,address=('0.0.0.0',5050))
+    ptvsd.wait_for_attach()
     print('ptvsd is started')
 except:
     print("NOT WORKING")
-# ptvsd.wait_for_attach()
+    
 print('hello')
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    name = "Yama"
+    name = YamaTest()
     return "Hello, " + name
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000)
